23309a5476eb50af0ca636f728bee2b0fb93a879
sylviawanjiru <sylvia.shiross@gmail.com>
2022-11-30 15:45:29 +0000

    12-fizzbuzz.py

---
+++ 0x00-python-hello_world/0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+n range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif num % 5 == 0:
+            print("Buzz ", end="")
+        elif num % 3 == 0:
+            print("Fizz ", end="")
+        else:
+            print(f"{num} ", end="")
