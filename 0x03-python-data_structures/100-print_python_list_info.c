2f85f681c7903c4524390ec14926aaec4ca924c0
sylviawanjiru <sylvia.shiross@gmail.com>
2022-12-05 15:56:14 +0000

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,9 +1,16 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return new_list
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
+
